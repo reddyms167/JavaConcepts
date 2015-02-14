99b706ef6d599df29397d842b7156bf473217af8
reddyms167 <reddyms167@gmail.com>
2015-02-13 23:07:28 -0600

    Added a serialVersionUID to the serializable class "Employee"

--- JavaConcepts/src/com/reddyms/concepts/Employee.java
+++ JavaConcepts/src/com/reddyms/concepts/Employee.java
@@ -3,6 +3,13 @@ package com.reddyms.concepts;
 import java.io.Serializable;
 
 public class Employee implements Serializable {
+	
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 56015538575841795L;
 	public String name;
 	public String address;
 	public transient int SSN;
